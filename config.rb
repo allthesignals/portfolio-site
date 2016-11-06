466d7ceeadb1b99f154e629c8834888cc9e9a4a0
allthesignals <wilburnforce@gmail.com>
2016-11-05 23:52:40 -0400

    Initial commit

---
+++ config.rb
@@ -0,0 +1,60 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+set :markdown_engine, :redcarpet
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
+
+helpers do
+
+  def markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML,
+      autolink: true,
+      filter_html: false,
+      escape_html: false
+    )
+    @markdown.render(content)
+  end
+
+end
